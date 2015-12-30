210d585e1aa588ebba5fdcdf18990ea385382347
unknown <ifollowyou@aliyun.com>
2015-12-30 09:15:27 +0800

    Go语言编程一书，第一章，15页的calc示例工程，使用os.Args对程序的参数个数判断错误。经查源码，os.Args是将程序名本身都当作一个参数的。

--- book-golang/calcproj/src/calc/calc.go
+++ book-golang/calcproj/src/calc/calc.go
@@ -11,18 +11,20 @@ var Usage = func() {
 
 func main() {
 	args := os.Args
-	if args == nil || len(args) < 2 {
+	fmt.Println("args=", args)
+	if args == nil || len(args) < 3 {
 		Usage()
 		return
 	}
-	switch args[0] {
+
+	switch args[1] {
 	case "add":
-		if len(args) != 3 {
+		if len(args) != 4 {
 			fmt.Println("USAGE: calc add <integer1><integer2>")
 			return
 		}
-		v1, err1 := strconv.Atoi(args[1])
-		v2, err2 := strconv.Atoi(args[2])
+		v1, err1 := strconv.Atoi(args[2])
+		v2, err2 := strconv.Atoi(args[3])
 		if err1 != nil || err2 != nil {
 			fmt.Println("USAGE: calc add <integer1><integer2>")
 			return
@@ -30,15 +32,17 @@ func main() {
 		ret := simplemath.Add(v1, v2)
 		fmt.Println("Result: ", ret)
 	case "sqrt":
-		if len(args) != 2 {
+		if len(args) != 3 {
 			fmt.Println("USAGE: calc sqrt <integer>")
 			return
 		}
-		v, err := strconv.Atoi(args[1])
+
+		v, err := strconv.Atoi(args[2])
 		if err != nil {
 			fmt.Println("USAGE: calc sqrt <integer>")
 			return
 		}
+
 		ret := simplemath.Sqrt(v)
 		fmt.Println("Result: ", ret)
 	default:
